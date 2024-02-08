388f7526730da75457aadc9b0a27b8b1bb970bc1
Amakran <abderrazaq.makran@algosup.com>
2024-02-08 11:48:20 +0100

    remove python we dont use it finnaly

--- CYOA/Python /game.py
+++
@@ -1,7 +0,0 @@
-name = input('Enter your name:')
-
-print( f'Greetings {name}! Welcome to the Chronicles of the Lost Kingdom!')
-
-path = input ('You find yourself standing at a crossroads. Which path will you choose?')
-if path == ("Take the dangerous shortcut"):
-    print("You bravely take the dangerous shortcut, facing various challenges along the way. After overcoming treacherous terrain and fierce adversaries, you finally arrive at the legendary bridge. As you approach, you notice a mysterious figure standing on the other side – it's Ryuk.")
